594a8bb0fe00baeda887fe344a1783d5145fcd09
pedrobarao <pedrobarao@pedrobaraos-MacBook-Pro.local>
2024-07-26 14:26:05 -0300

    feat: add user model

---
+++ models/user.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+// Define the Currency type
+type Currency string
+
+// Define the possible values for Currency
+const (
+  IC  Currency = "IC"
+  USD Currency = "USD"
+  BRL Currency = "BRL"
+  ETH Currency = "ETH"
+  BTC Currency = "BTC"
+)
+
+// Define the UserStatus type
+type UserStatus string
+
+// Define the possible values for UserStatus
+const (
+  Review  UserStatus = "review"
+  Success UserStatus = "success"
+  Failed  UserStatus = "failed"
+  Approved UserStatus = "approved"
+)
+
+type User struct {
+  Id        uuid.UUID      `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+  Name  string `json:"name"`
+  Email  string `json:"email"` 
+  PublicKey string `json:"public_key"`
+  Status   *UserStatus `json:"status"`
+  Balance   decimal.Decimal `json:"balance" gorm:"type:decimal(20,2)"`
+  Currency *Currency    `json:"currency"`
+  CreatedAt int    `json:"created_at"`
+  UpdatedAt int    `json:"updated_at"`
+}
