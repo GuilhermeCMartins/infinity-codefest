14a1b50e8f278a4b397f7de4b633d04ba1c3e277
pedrobarao <pedrobarao@pedrobaraos-MacBook-Pro.local>
2024-07-29 15:21:41 -0300

    refactor: add health check and remove unused characters

--- apps/user/user.controller.go
+++ apps/user/user.controller.go
@@ -12,117 +12,109 @@ import (
 func SetupUserRoutes(router *gin.Engine, db *gorm.DB) {
 	u := router.Group("/users")
 
-	{
-		{
-			u.GET("/", func(c *gin.Context) {
-				users, count, err := FindAllUsers(db)
-				
-				if err != nil {
-								c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-								return
-				}
-				
-				c.JSON(http.StatusOK, gin.H{
-						"users": users,
-						"count": count,
-				})
-			})
+	u.GET("", func(c *gin.Context) {
+		users, count, err := FindAllUsers(db)
+		
+		if err != nil {
+						c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+						return
 		}
-		{
-			u.GET("/:id", func(c *gin.Context) {
-				id := c.Param("id")
-				userId, _ := uuid.Parse(id)
+		
+		c.JSON(http.StatusOK, gin.H{
+				"users": users,
+				"count": count,
+		})
+	})
 
-				user, err := FindUserById(db, userId)
+	u.GET("/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		userId, _ := uuid.Parse(id)
 
-				if err == gorm.ErrRecordNotFound {
-					c.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
-					return
-				}
+		user, err := FindUserById(db, userId)
 
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-					return
-				}
-				
-				c.JSON(http.StatusOK, user)
-			})
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+			return
 		}
-		{
-			u.GET("/:id/transactions", func(c *gin.Context) {
-				id := c.Param("id")
-				userId, _ := uuid.Parse(id)
-				transactions, count, err := FindUserTransactions(db, userId)
-				
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-					return
-				}
-				
-				c.JSON(http.StatusOK, gin.H{
-						"user_id": userId,
-						"count": count,
-						"transactions": transactions,
-				})
-			})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		{
-			u.GET("/:id/transactions/:tx", func(c *gin.Context) {
-				id := c.Param("id")
-				txId := c.Param("tx")
-				userId, _ := uuid.Parse(id)
-				txUUID, _ := uuid.Parse(txId)
-				transaction, sender, err := FindUserTransactionByTransactionId(db, userId, txUUID)
+		
+		c.JSON(http.StatusOK, user)
+	})
 
-				if err == gorm.ErrRecordNotFound {
-					c.JSON(http.StatusNotFound, gin.H{"message": "User or transaction not found"})
-					return
-				}
-				
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-					return
-				} 
-
-				c.JSON(http.StatusOK, gin.H{
-						"user_id": userId,
-						"id": txUUID,
-						"sender": sender,
-						"transaction": transaction,
-				})
-			})
+	u.GET("/:id/transactions", func(c *gin.Context) {
+		id := c.Param("id")
+		userId, _ := uuid.Parse(id)
+		transactions, count, err := FindUserTransactions(db, userId)
+		
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		{
-			u.GET("/:id/transactions/status/:status", func(c *gin.Context) {
-				id := c.Param("id")
-				status := c.Param("status")
-				userId, _ := uuid.Parse(id)
-				transactionStatus := models.TransactionStatus(status)
-				transactions, count, err := FindUserTransactionsByStatus(db, userId, transactionStatus)
+		
+		c.JSON(http.StatusOK, gin.H{
+				"user_id": userId,
+				"count": count,
+				"transactions": transactions,
+		})
+	})
 
-				//TO-DO: Refactor this to use a switch statement
-				if (status != "approved" && status != "success" && status != "failed" && status != "review") {
-					c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid status"})
-					return
-				}
-				
-				if err == gorm.ErrRecordNotFound {
-					c.JSON(http.StatusNotFound, gin.H{"message": "Invalid status"})
-					return
-				}
+	u.GET("/:id/transactions/:tx", func(c *gin.Context) {
+		id := c.Param("id")
+		txId := c.Param("tx")
+		userId, _ := uuid.Parse(id)
+		txUUID, _ := uuid.Parse(txId)
+		transaction, sender, err := FindUserTransactionByTransactionId(db, userId, txUUID)
 
-				if err != nil {
-					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-					return
-				}
-				
-				c.JSON(http.StatusOK, gin.H{
-						"user_id": userId,
-						"status": status,
-						"count": count,
-						"transactions": transactions,
-				})
-			})
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"message": "User or transaction not found"})
+			return
 		}
-	}
+		
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		} 
+
+		c.JSON(http.StatusOK, gin.H{
+				"user_id": userId,
+				"id": txUUID,
+				"sender": sender,
+				"transaction": transaction,
+		})
+	})
+
+	u.GET("/:id/transactions/status/:status", func(c *gin.Context) {
+		id := c.Param("id")
+		status := c.Param("status")
+		userId, _ := uuid.Parse(id)
+		transactionStatus := models.TransactionStatus(status)
+		transactions, count, err := FindUserTransactionsByStatus(db, userId, transactionStatus)
+
+		//TO-DO: Refactor this to use a switch statement
+		if (status != "approved" && status != "success" && status != "failed" && status != "review") {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid status"})
+			return
+		}
+		
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Invalid status"})
+			return
+		}
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		
+		c.JSON(http.StatusOK, gin.H{
+				"user_id": userId,
+				"status": status,
+				"count": count,
+				"transactions": transactions,
+		})
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -4,12 +4,24 @@ import (
 	"myapp/apps/transactions"
 	"myapp/apps/user"
 	"myapp/db"
+	middleware "myapp/middlewares"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 	db := db.Init()
+	router.HandleMethodNotAllowed = true
+
+	router.NoMethod(middleware.MethodCheckHandler())
+	router.NoRoute(middleware.NotFoundHandler())
+	
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+				"message": "ping",
+		})
+	})
 
 	user.SetupUserRoutes(router, db)
 	transactions.SetupTransactionsRoutes(router, db)
